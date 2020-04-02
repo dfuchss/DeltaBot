c314b98272c93e1f1b930c3b3d426d99e3190639
Dominik Fuchß <develop@fuchss.org>
2020-04-03 01:18:11 +0200

    Fixed command order

--- system_commands.py
+++ system_commands.py
@@ -36,12 +36,20 @@ async def __handling_template(self, cmd: str, message: Message, func_dm: Handlin
 
 
 async def handle_system(self, message: Message) -> bool:
+    if await __handling_template(self, "\\listen-all", message,
+                                 lambda: send(message.author, message.channel, self, f"Immer Antworten-Modus ist jetzt {'an' if (self.config.toggle_listen_all()) else 'aus'}"),
+                                 lambda: send(message.author, message.channel, self, "Du bist nicht authorisiert!"),
+                                 lambda: send(message.author, message.channel, self, f"Immer Antworten-Modus ist jetzt {'an' if (self.config.toggle_listen_all()) else 'aus'}")
+                                 ):
+        return True
+
     if await __handling_template(self, "\\listen", message,
                                  lambda: send(message.author, message.channel, self, "Ich höre Dich schon!"),
                                  lambda: send(message.author, message.channel, self, "Du bist nicht authorisiert!"),
                                  lambda: self.channels.append(message.channel.id)
                                  ):
         return True
+    
     if await __handling_template(self, "\\admin", message,
                                  lambda: send(message.author, message.channel, self, f"Für DM nicht sinnvoll."),
                                  lambda: send(message.author, message.channel, self, "Du bist nicht authorisiert!"),
@@ -60,13 +68,6 @@ async def handle_system(self, message: Message) -> bool:
                                  lambda: send(message.author, message.channel, self, f"Sprachausgabe ist jetzt {'an' if (self.config.toggle_tts()) else 'aus'}"),
                                  lambda: send(message.author, message.channel, self, "Du bist nicht authorisiert!"),
                                  lambda: send(message.author, message.channel, self, f"TTS ist jetzt: {'an' if (self.config.toggle_tts()) else 'aus'}")
-                                 ):
-        return True
-
-    if await __handling_template(self, "\\listen-all", message,
-                                 lambda: send(message.author, message.channel, self, f"Immer Antworten-Modus ist jetzt {'an' if (self.config.toggle_listen_all()) else 'aus'}"),
-                                 lambda: send(message.author, message.channel, self, "Du bist nicht authorisiert!"),
-                                 lambda: send(message.author, message.channel, self, f"Immer Antworten-Modus ist jetzt {'an' if (self.config.toggle_listen_all()) else 'aus'}")
                                  ):
         return True
 
