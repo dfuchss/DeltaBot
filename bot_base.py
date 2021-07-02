f49e634f09cd7a5a0ff6793358f82ff23bc98b5f
Dominik Fuchß <develop@fuchss.org>
2021-07-02 23:43:09 +0200

    Added lazy boot of NLU

--- bot_base.py
+++ bot_base.py
@@ -171,9 +171,17 @@ class BotBase(Client):
     def __init__(self):
         super().__init__()
         self.config: Configuration = Configuration()
-        self.nlu: NLUService = NLUService(self.config)
+        # Load NLU Lazy ..
+        self.nlu: Union[NLUService, Callable] = lambda: NLUService(self.config)
         self.scheduler: BotScheduler = BotScheduler(self.loop)
         self._init_deletions()
+
+    def __getattribute__(self, name: str):
+        attr = super(BotBase, self).__getattribute__(name)
+        if name == "nlu" and callable(attr):
+            nlu = self.nlu = attr()
+            return nlu
+        return attr
 
     @staticmethod
     def log(message: Message) -> None:
