b8c6f3fa13a4b20cf6fafbf4078c51797528cd16
Dominik Fuchß <develop@fuchss.org>
2021-10-03 15:25:25 +0200

    Put erase into daemon

--- src/main/kotlin/org/fuchss/deltabot/command/admin/Erase.kt
+++ src/main/kotlin/org/fuchss/deltabot/command/admin/Erase.kt
@@ -1,10 +1,12 @@
 package org.fuchss.deltabot.command.admin
 
+import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent
 import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import org.fuchss.deltabot.command.BotCommand
 import org.fuchss.deltabot.command.CommandPermissions
 import org.fuchss.deltabot.utils.fetchHistory
+import org.fuchss.deltabot.utils.logger
 
 class Erase : BotCommand {
     override val permissions: CommandPermissions get() = CommandPermissions.GUILD_ADMIN
@@ -15,20 +17,25 @@ class Erase : BotCommand {
     }
 
     override fun handle(event: SlashCommandEvent) {
-        val reply = event.reply("Starting the deletion of all messages ...").setEphemeral(true).complete()
+        event.reply("Starting the deletion of all messages ... if not all messages will be deleted restart your client or ask the bot owner").setEphemeral(true).queue()
 
-        val retrieveMax = 10
+        val deletion = Thread { deleteMessages(event.channel) }
+        deletion.isDaemon = true
+        deletion.start()
+    }
 
+    private fun deleteMessages(channel: MessageChannel) {
+        val retrieveMax = 20
         try {
-            var history = event.channel.history.fetchHistory(retrieveMax)
+            var history = channel.history.fetchHistory(retrieveMax)
             while (history.isNotEmpty()) {
                 for (m in history) {
-                    m.delete().complete()
+                    m.delete().queue()
                 }
-                history = event.channel.history.fetchHistory(retrieveMax)
+                history = channel.history.fetchHistory(retrieveMax)
             }
         } catch (e: Exception) {
-            reply.editOriginal("An error occurred while deleting messages: ${e.message}").queue()
+            logger.error(e.message, e)
         }
     }
 }
