128e366bfb91d771bcd6a1c8f500bdd0ba1d9884
Dominik Fuchß <develop@fuchss.org>
2022-03-13 14:36:34 +0100

    Added color to server role command

--- src/main/kotlin/org/fuchss/deltabot/command/admin/ServerRoles.kt
+++ src/main/kotlin/org/fuchss/deltabot/command/admin/ServerRoles.kt
@@ -3,6 +3,7 @@ package org.fuchss.deltabot.command.admin
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.exceptions.PermissionException
+import net.dv8tion.jda.api.interactions.commands.Command
 import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction
 import net.dv8tion.jda.api.interactions.commands.build.Commands
@@ -14,11 +15,30 @@ import org.fuchss.deltabot.command.CommandPermissions
 import org.fuchss.deltabot.command.GuildCommand
 import org.fuchss.deltabot.utils.extensions.logger
 import org.fuchss.deltabot.utils.extensions.translate
+import java.awt.Color
 
 /**
  * A [BotCommand] that manages (add or remove) the roles of a server with linked channels.
  */
 class ServerRoles : GuildCommand {
+    companion object {
+        val COLORS: Map<String, Int> = mapOf(
+            "White" to Color.white.rgb,
+            "Light Gray" to Color.lightGray.rgb,
+            "Gray" to Color.gray.rgb,
+            "Dark Gray" to Color.darkGray.rgb,
+            "Black" to Color.black.rgb,
+            "Red" to Color.red.rgb,
+            "Pink" to Color.pink.rgb,
+            "Orange" to Color.orange.rgb,
+            "Yellow" to Color.yellow.rgb,
+            "Green" to Color.green.rgb,
+            "Magenta" to Color.magenta.rgb,
+            "Cyan" to Color.cyan.rgb,
+            "Blue" to Color.blue.rgb
+        )
+    }
+
     override val permissions: CommandPermissions get() = CommandPermissions.GUILD_ADMIN
 
     override fun createCommand(guild: Guild): SlashCommandData {
@@ -27,7 +47,10 @@ class ServerRoles : GuildCommand {
             SubcommandData("add", "add a new role with text and voice channel").addOptions(
                 OptionData(OptionType.STRING, "name", "the name of the role").setRequired(true),
                 OptionData(OptionType.BOOLEAN, "text", "create text channel? (default: true)").setRequired(false),
-                OptionData(OptionType.BOOLEAN, "voice", "create voice channel? (default: true)").setRequired(false)
+                OptionData(OptionType.BOOLEAN, "voice", "create voice channel? (default: true)").setRequired(false),
+                OptionData(OptionType.STRING, "color", "the color to associate with the new role (default: none)").setRequired(false).addChoices(
+                    COLORS.toSortedMap().map { c -> Command.Choice(c.key, c.value.toString()) }
+                )
             ),
 
             SubcommandData("del", "remove role and connected text and voice channels").addOptions(
@@ -49,6 +72,7 @@ class ServerRoles : GuildCommand {
         val name = event.getOption("name")?.asString?.trim() ?: ""
         val text = event.getOption("text")?.asBoolean ?: true
         val voice = event.getOption("voice")?.asBoolean ?: true
+        val color = event.getOption("color")?.asString?.toIntOrNull()
 
         if (name.isBlank() || !name.matches(Regex("[A-Za-z0-9 ]+"))) {
             event.reply("You need to supply a name with no special characters!".translate(event)).setEphemeral(true).queue()
@@ -64,7 +88,7 @@ class ServerRoles : GuildCommand {
 
         val hook = event.deferReply().complete()
         val everyone = guild.publicRole
-        val role = guild.createRole().setName(name).setMentionable(true).complete()
+        val role = guild.createRole().setName(name).setMentionable(true).setColor(color).complete()
         val group = guild.createCategory(name)
             .addMemberPermissionOverride(guild.selfMember.idLong, listOf(Permission.VIEW_CHANNEL), null)
             .addRolePermissionOverride(role.idLong, listOf(Permission.VIEW_CHANNEL), null)
